6656d8ac38f769cc78bb209ea6b2838e5e5dfea8
Andrey Hicks <AHicks@bitcentral.net>
2019-03-15 16:03:02 -0700

    Added source file and function

--- jlog.go
+++ jlog.go
@@ -1,60 +1,50 @@
 package jlog
 
 import (
-
-    "fmt"
-    "time"
-    "encoding/json"
-
+	"encoding/json"
+	"log"
+	"runtime"
 )
 
 const (
-
-	INFO = "info"
-	ERROR = "error"
-	INIT = "Init"
-
+	INFO    = "info"
+	ERROR   = "error"
+	WARNING = "warning"
+	DEBUG   = "debug"
+	TRACE   = "trace"
+	INIT    = "Init"
 )
 
 type Fields map[string]interface{}
 
 type logData struct {
-
-	Timestamp string  				`json:"time"`
-	Src string  			 		`json:"src"`
-	Method string   				`json:"method"`
-	MsgType string 					`json:"type"`
-	Msg string   					`json:"message"`
-	Data map[string]interface{}  	`json:"data"`
-
+	Src     string                 `json:"src"`
+	FName   string                 `json:"fname"`
+	MsgType string                 `json:"type"`
+	Msg     string                 `json:"message"`
+	Data    map[string]interface{} `json:"data"`
 }
 
-func Log(stdOut bool, logOut bool, src string, method string, msgType string, msg string, data map[string]interface{}) {
+func Log(msgType string, msg string, data map[string]interface{}) {
+
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
 
 	var lData logData
 
-	t := time.Now()
-	lData.Timestamp = t.String()
-	lData.Src = src
-	lData.Method = method
+	_, lData.Src, _, _ = runtime.Caller(1)
+	lData.FName = frame.Function
 	lData.MsgType = msgType
 	lData.Msg = msg
 	lData.Data = data
 
-	// marshal to json
 	data_json, err := json.Marshal(lData)
-
-	// handle errors
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
+	} else {
+		log.Println(string(data_json))
 	}
 
-	if stdOut {
-		fmt.Println(string(data_json))
-	}
-
-	if logOut {
-		// create file to log
-	}
-    
 }
